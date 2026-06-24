72ceafd7bd70ebd7f3c065ba7d5af825b411146d
Mukesh Kumar <mk8701952@gmail.com>
2026-06-24 10:17:43 +0530

    auto update

--- src/app/about/AboutPage.jsx
+++ src/app/about/AboutPage.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Breadcrumb from "@/components/Breadcrumb";
+import DisclaimerSection from "./DisclaimerSection";
 export default function AboutPage() {
 
   return (
@@ -219,7 +220,7 @@ export default function AboutPage() {
         </div>
 
       </div>
-
+<DisclaimerSection/>
     </section>
 
   );
